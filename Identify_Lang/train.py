7690fc0e690cad8a58fc3b3fe598e9696405a7bf
barnawalabhinav <abhinav.barnawal@iitd.ac.in>
2024-02-02 21:45:03 +0530

    Adding naive bayes baseline

---
+++ Identify_Lang/train.py
@@ -0,0 +1,53 @@
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+from evaluation import compute_macro_f1_score, compute_micro_f1_score
+
+
+def load_data(data_path):
+    df = pd.read_json(data_path)
+    return df
+
+
+def naive_bayes_baseline(dataframe, model_path):
+    pipeline = Pipeline([
+        # ('tfidf', TfidfVectorizer()),
+        ('cntvec', CountVectorizer()),
+        ('mnb', MultinomialNB())
+    ])
+    pipeline.fit(dataframe['text'].values, dataframe['langid'].values)
+    with open(model_path, 'wb') as f:
+        pickle.dump(pipeline, f)
+
+
+def train(data_path, model_path):
+    df = load_data(data_path)
+
+    naive_bayes_baseline(df, model_path)
+
+    print('------------------ Training Done! ------------------')
+
+
+def predict(data_path, model_path):
+    df = load_data(data_path)
+
+    with open(model_path, 'rb') as f:
+        pipeline = pickle.load(f)
+    y_pred = pipeline.predict(df['text'].values)
+
+    y_true = df['langid'].values
+    micro_f1 = compute_micro_f1_score(y_pred, y_true)
+    macro_f1 = compute_macro_f1_score(y_pred, y_true)
+    accuracy = np.mean(y_pred == y_true)
+    print(f'Micro F1: {micro_f1}')
+    print(f'Macro F1: {macro_f1}')
+    print(f'Accuracy: {accuracy}')
+
+
+if __name__ == '__main__':
+    train('data/train.json', 'models/model.pkl')
+    predict('data/valid.json', 'models/model.pkl')
